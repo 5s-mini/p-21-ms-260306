3c3fa668dda6a30d62e4c870fa6c79ca36da67ba
5s-mini <dnclsehd123@naver.com>
2026-03-07 16:48:32 +0900

    feat: 최신순으로 데이터 조회

--- src/main/java/com/back/question/QuestionService.java
+++ src/main/java/com/back/question/QuestionService.java
@@ -5,10 +5,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import java.util.ArrayList;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
@@ -38,7 +40,9 @@ public class QuestionService {
     }
 
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
 }
