637f54f09e5777f1b1628e79b31ebf36c6ca7033
5s-mini <dnclsehd123@naver.com>
2026-03-12 17:51:03 +0900

    feat: 답변 앵커 추가

--- src/main/java/com/back/answer/Answer.java
+++ src/main/java/com/back/answer/Answer.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
