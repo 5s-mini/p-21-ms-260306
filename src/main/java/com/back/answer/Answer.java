9265fce745f78fc9338238b85e0644f2a17c827f
5s-mini <dnclsehd123@naver.com>
2026-03-12 17:41:09 +0900

    feat: 엔티티에 속성 추가

--- src/main/java/com/back/answer/Answer.java
+++ src/main/java/com/back/answer/Answer.java
@@ -1,11 +1,13 @@
 package com.back.answer;
 
 import com.back.question.Question;
+import com.back.user.SiteUser;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -28,4 +30,7 @@ public class Answer {
     private Question question;
 
     private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
